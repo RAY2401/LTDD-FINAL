dae5a3f19d92f95ac0ccd775da966d887c02f2db
RAY2401 <90088094+RAY2401@users.noreply.github.com>
2022-05-11 10:32:53 +0700

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNkorhiJX_YwF9fZ9e1fHNQ9mUPrWyp7g",
+  authDomain: "fir-5af7d.firebaseapp.com",
+  projectId: "fir-5af7d",
+  storageBucket: "fir-5af7d.appspot.com",
+  messagingSenderId: "295697953401",
+  appId: "1:295697953401:web:9964ae97a0f37f38471aa8",
+};
